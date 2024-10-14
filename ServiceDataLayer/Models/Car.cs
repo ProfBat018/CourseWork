63d7d450358f8ad6d450498cfc2dfeadc1af1ac6
Elvin <profbat018@gmail.com>
2024-10-14 20:38:59 +0400

    Update Car.cs

--- ServiceDataLayer/Models/Car.cs
+++ ServiceDataLayer/Models/Car.cs
@@ -8,14 +8,16 @@ namespace ServiceDataLayer.Models
 {
     public class Car
     {
-        public Guid Id { get; set; }
+        public Guid Id { get; set; } // NewGuid 
 
-        public string Make { get; set; } = string.Empty;
+        public string Make { get; set; } = string.Empty; // Ubrat 
 
         public string Model { get; set; } = string.Empty;
 
-        public string LicensePlate { get; set; } = string.Empty;
+        public string LicensePlate { get; set; } = string.Empty; // Не забудь про Validation. Посмотри Validation Service в моем проекте 
 
+        // Машина не должна хранить User, сделай отдельную таблицу UserCars 
+ 
         public Guid UserId { get; set; }
         public User User { get; set; }
         public Guid StatusId { get; set; }
@@ -23,4 +25,13 @@ namespace ServiceDataLayer.Models
         public Role Role { get; set; }
         public ICollection<Car> Cars { get; set; }
     }
+
+public class UserCars {
+ public Guid Id { get; set; } // NewGuid 
+public Guid UserId {get; set; } 
+public Guid CarId {get; set; } 
+
+// Не забудь про сами ссылки для будущего İnnerJoin, 
+    // В самих таблицах будет ICollection из UserCars. 
+}
 }
