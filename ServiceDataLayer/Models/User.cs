007c93aab1594c642947e01560ba14fbb1891c93
Elvin <profbat018@gmail.com>
2024-10-14 19:46:17 +0400

    Update User.cs

--- ServiceDataLayer/Models/User.cs
+++ ServiceDataLayer/Models/User.cs
@@ -8,7 +8,7 @@ namespace ServiceDataLayer.Models
 {
     public class User
     {
-        public Guid Id { get; set; }
+        public Guid Id { get; set; } = Guid.NewGuid();
 
         public string Username { get; set; } = string.Empty;
 
@@ -21,11 +21,28 @@ namespace ServiceDataLayer.Models
         public string Phone { get; set; } = string.Empty;
         
         public string Password { get; set; } = string.Empty;
-        public Guid RoleId { get; set; }
-        public Role Role { get; set; }
+      
+      //  public Guid RoleId { get; set; }
+     //   public Role Role { get; set; }
+      
         public ICollection<Car> Cars { get; set; }
 
 
+    }
+
+    // Роли хранятся в отдельной UserRole 
+    // не забудь доабить DbSet в context
+
+    public class UserRole  {
+    public Guid RoleId { get; set; } = Guid.NewGuid();  // это тоже foreignkey 
+        public Guid UserId { get; set; } = Guid.NewGuid(); // это ForeignKey
+        
+    }
+
+    public class Role {
+          public Guid RoleId { get; set; } = Guid.NewGuid();  
+        public string roleName {get; set; }
+        
     }
 
 }
